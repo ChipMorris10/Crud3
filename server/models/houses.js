34f586cee65bafc492633328e100c18219fd4cc9
ChipMorris10 <goldcomac@mac.com>
2015-09-16 18:51:03 -0600

    finished and tested backend

---
+++ server/models/houses.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var House = new Schema({
+    location:String,
+    price: Number,
+    garage: Boolean
+});
+
+
+mongoose.connect (process.env.MONGO_URI || 'mongodb;//localhost/housees');
+
+
+module.exports = mongoose.model('houses', House);  // making mongoose database available to the rest of the app. The rest of the app uses this as a resource.
