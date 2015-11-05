4ee050bf720224037520e80af64341bd4e69dc21
ChipMorris10 <goldcomac@mac.com>
2015-11-04 19:59:36 -0700

    cleaning up files

--- server/models/houses.js
+++ server/models/houses.js
@@ -9,7 +9,7 @@ var House = new Schema({
 });
 
 
-mongoose.connect (process.env.MONGO_URI || 'mongodb;//localhost/housees');
+mongoose.connect (process.env.MONGO_URI || 'mongodb://localhost/houses');
 
 
 module.exports = mongoose.model('houses', House);  // making mongoose database available to the rest of the app. The rest of the app uses this as a resource.
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,32 +4,61 @@ var House = require('../models/houses');
 
 // GET ALL house
 router.get('/houses', function(req, res, next) {
-    res.send("Hey Johnny!");
+   House.find(function(err, housesData) {
+     if(err) {
+            res.json({'message': err});
+        } else {
+            res.json(housesData);
+        }
+    });
 });
 
 // GET SINGLE house
-router.post('/houses', function (req, res, next) {
-
+router.get('/house/:id', function (req, res, next) {
+    House.findById(req.params.id, function(err, houseData) {
+        if(err) {
+            res.json({'message': err});
+        } else {
+            res.json(houseData);
+        }
+    });
 });
 
 
 // POST ALL houses
 router.post('/houses', function (req, res, next) {
-
+    newHouse = new House ( {
+        location:req.body.location,
+        price:req.body.price,
+        garage:req.body.garage
+    });
+    newHouse.save(function(err, houseData) {
+        if(err) {
+            res.json({'message': err});
+        } else {
+            res.json(houseData);
+        }
+    });
 });
 
-// GET ONE house
-router.post('/houses', function (req, res, next) {
-
-});
-
-// UPDATE ONE house
-router.post('/houses', function (req, res, next) {
-
+// PUT (Update) ONE house
+router.put('/houses/:id', function (req, res, next) {
+    var update = {
+        location:req.body.location,
+        price:req.body.price,
+        garage:req.body.price
+    };
+    House.findByIdAndUpdate(req.params.id, update, function(err,houseData) {
+        if(err) {
+            res.json({'message': err});
+        } else {
+            res.json(houseData);
+        }
+    });
 });
 
 // DELETE ONE house
-router.post('/houses', function (req, res, next) {
+router.delete('/houses', function (req, res, next) {
 
 });
 
